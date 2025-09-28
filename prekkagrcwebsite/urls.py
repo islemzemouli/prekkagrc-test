0606b46cc13d990eb585b818e42f5b2d9f2f6082
islemzemouli <zemouliislem01@gmail.com>
2025-09-28 12:58:52 +0200

    Update urls.py

--- prekkagrcwebsite/urls.py
+++ prekkagrcwebsite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core import views
-from core.views import dummy_admin
+from core.apps import CoreConfig
 
 urlpatterns = [
     path('admin/', dummy_admin.urls),  # admin accessible without login
